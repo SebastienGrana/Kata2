7037d9b5e3d6951c161d9b4438a5513f5204e24f
SebastienGrana <granarolo.sebastien@gmail.com>
2016-05-24 14:11:39 +0200

    All test ok
    test 1 String ok ; test full string ok;

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,20 @@
+import java.util.StringTokenizer;
 
 public class Calculator {
-public int add(String numbers){
-	int result =0;
-	if (numbers.isEmpty()) {
-		
+	public int add(String numbers){
+		int result =0;
+		if (numbers.isEmpty()) {
+			
+		}else if(numbers.length() == 1){
+			result = Integer.parseInt(numbers);
+		}else{
+			StringTokenizer st = new StringTokenizer(numbers, ",");
+			
+			while (st.hasMoreElements()) {
+				 result += Integer.parseInt( st.nextToken());
+				
+			}
+		}
+		return result;
 	}
-	return result;
-}
 }
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -5,10 +5,23 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void test() {
+	public void testEmptyString() {
 		Calculator calc = new Calculator();
 		int res = calc.add("");
 		assertEquals(0,res);
 	}
+	
+	@Test
+	public void testOneString() {
+		Calculator calc = new Calculator();
+		int res = calc.add("1");
+		assertEquals(1,res);
+	}
 
+	@Test
+	public void testFullString() {
+		Calculator calc = new Calculator();
+		int res = calc.add("1,2,3");
+		assertEquals(6,res);
+	}
 }
