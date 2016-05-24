8c82fdbecd005d897cc4dfd3dd786071483b9bd5
BeYouss <youssef.boubekri@gmail.com>
2016-05-24 14:48:31 +0200

    more comments!!!

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,10 +4,11 @@ public class Calculator {
 	public int add(String numbers) {
 		//final result
 		int result = 0;
-		
+		//single number
 		if (numbers.length() == 1) {
 			result = Integer.parseInt(numbers);
 		} else {
+			//loop through the strings tokens and sum up
 			StringTokenizer stringTokenizer = new StringTokenizer(numbers, ",");
 			
 			while (stringTokenizer.hasMoreElements()) {
